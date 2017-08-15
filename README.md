88731a6f274f1e34191aa4221c3b2ba3d29b4dd7
sonnenfeld269 <david.sonnenfeld269@gmail.com>
2017-08-15 12:32:06 +0200

    update map with circles and rectangles

--- README.md
+++ README.md
@@ -1,15 +1,56 @@
 <!-- TOC -->
 
 - [Activity Map](#activity-map)
-    - [Technologies, Frameworks, Libraries](#technologies-frameworks-libraries)
-    - [Tutorials, Documentation](#tutorials-documentation)
-    - [Implementation](#implementation)
-        - [react-leaflet vs react-map-gl](#react-leaflet-vs-react-map-gl)
-            - [Leaflet](#leaflet)
-            - [Map-GL](#map-gl)
+  - [Basics](#basics)
+    - [Vector tiles vs Raster Tiles](#vector-tiles-vs-raster-tiles)
+    - [Possible to host vector and raster tiles yourself?](#possible-to-host-vector-and-raster-tiles-yourself)
+    - [Leaflet vs Mapbox GL JS](#leaflet-vs-mapbox-gl-js)
+    - [Styling raster vs vector tiles](#styling-raster-vs-vector-tiles)
+  - [Descision](#descision)
+  - [Technologies, Frameworks, Libraries](#technologies-frameworks-libraries)
+  - [Tutorials, Documentation](#tutorials-documentation)
+  - [Implementation](#implementation)
+    - [react-leaflet vs react-map-gl](#react-leaflet-vs-react-map-gl)
+      - [Leaflet](#leaflet)
+      - [Map-GL](#map-gl)
 
 <!-- /TOC -->
 # Activity Map
+
+## Basics
+
+![concept](/doc/anatomy.png)
+
+### Vector tiles vs Raster Tiles
+Raster tiles are images that are generated on the server and then sent to the client. Vector tiles are rendered on the fly, clientside. Which makes them faster, through WebGL. 
+
+Why we still should try a raster version?
+Vector tiles  require OpenGL/WebGL/DirectX which can be an issue on mobile devices. So a raster tile version is good to have.
+
+
+### Possible to host vector and raster tiles yourself?
+
+Yes. In 
+[these docs](#https://openmaptiles.org/docs/) you can find implementation examples for both. 
+link
+
+### Leaflet vs Mapbox GL JS
+
+For now Leaflet should be used for raster tiles, because it is not possible yet to serve vector styles to a leaflet vector grid component. And the leaflet-mabox-gl-js library is still experimental.
+
+Mapbox GL JS has no distinction between baselayers and overlay layers. This means that map details like labels and icons and elements like streets and buildings can be modified with JavaScript, just like overlays in earlier mapping libraries. Each layer provides rules about how certain data should be drawn in the browser, and the renderer uses these layers to actually draw the map on the screen.
+
+
+### Styling raster vs vector tiles
+
+
+
+## Descision
+
+Prefer to go both ways. Make a prototype of each one. 
+
+* Leaflet with hosting remote raster tiles
+* Mapbox GL JS with hosting own vector tiles
 
 ## Technologies, Frameworks, Libraries
 * react
@@ -31,6 +72,6 @@
 #### Map-GL
 * supports vector tiles
 * can use simple style url for integrating styles
-works great with openmaptiles (another free open source vector tiles provider)
+works great with openmaptiles (free open source vector tiles provider)
 
 tbc.
--- src/map-gl/Map.js
+++ src/map-gl/Map.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Map, Marker, Popup, TileLayer, Circle, Rectangle } from 'react-leaflet';
-import { Icon } from 'leaflet';
-import VectorGridLayer from './VectorGridLayer';
+import MapGL from 'react-map-gl';
+
 import homeIconUrl from '../images/home.png';
 import gardenJson from '../data/garden.json';
 import 'isomorphic-fetch';
 
-const homeIcon = new Icon({
+/*const homeIcon = new Icon({
   iconUrl: homeIconUrl,
   iconSize: [55, 55], // size of the icon
-});
+});*/
+
 
 export default class SimpleExample extends React.Component {
   constructor(props) {
@@ -17,9 +17,10 @@ export default class SimpleExample extends React.Component {
     this.state = {
       lat: props.fnData.user.userPosition.lat,
       lng: props.fnData.user.userPosition.lng,
-      zoom: 14
+      zoom: 16,
     };
   }
+
 
   render() {
     const position = [this.state.lat, this.state.lng];
@@ -31,27 +32,28 @@ export default class SimpleExample extends React.Component {
             iconUrl: this.props.fnData.pointsOfInterest[0].iconUrl,
             iconSize: [30, 30],
           })}>
-          <Popup><span>{garden.name}</span></Popup>
+          <Popup><span>gortn</span></Popup>
         </Marker>
       )
     );
-    
     return (
       <div>
-        <Map style={{ height: '400px', width: '400px' }} center={position} zoom={this.state.zoom} attributionControl={true} ref="map">
-          <TileLayer
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url='https://api.mapbox.com/styles/v1/sonnenfeld269/cj4zk91ut0sjo2rnfn9plcuq0/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic29ubmVuZmVsZDI2OSIsImEiOiJjajQwMmlvaTEwOXFzMnF0MTM3MjZuY2U0In0.T1X3PSCiYOI87qNi6AYBYw'
-          />
+        <Map style={{ height: '400px', width: '400px' }} center={position} zoom={this.state.zoom} attributionControl={true}>
+          <VectorGridLayer />
           {/*<TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
           />*/}
+          <Marker position={position} icon={homeIcon}>
+            <Popup>
+              <span>{this.props.fnData.user.name}</span>
+            </Popup>
+          </Marker>
           <Marker
             position={this.props.fnData.events[0].position}
             icon={new Icon({
               iconUrl: this.props.fnData.events[0].iconUrl,
-              iconSize: [30, 30],
+              iconSize: [40, 40],
             })}>
             <Popup>
               <span>{this.props.fnData.events[0].name}</span>
@@ -61,7 +63,7 @@ export default class SimpleExample extends React.Component {
             position={this.props.fnData.events[1].position}
             icon={new Icon({
               iconUrl: this.props.fnData.events[1].iconUrl,
-              iconSize: [30, 30],
+              iconSize: [40, 40],
             })}>
             <Popup>
               <span>{this.props.fnData.events[1].name}</span>
@@ -71,34 +73,15 @@ export default class SimpleExample extends React.Component {
             position={this.props.fnData.pointsOfInterest[0].position}
             icon={new Icon({
               iconUrl: this.props.fnData.pointsOfInterest[0].iconUrl,
-              iconSize: [30, 30],
+              iconSize: [40, 40],
             })}>
             <Popup>
               <span>{this.props.fnData.pointsOfInterest[0].name}</span>
             </Popup>
           </Marker>
           {gardens}
-          <MyCircle data={this.state} />
-          <Rectangle bounds={[[this.state.lat-0.01,this.state.lng-0.01],[this.state.lat+0.01,this.state.lng+0.01]]} fillColor="red" />
-          <Marker position={position} icon={homeIcon}>
-            <Popup>
-              <span>{this.props.fnData.user.name}</span>
-            </Popup>
-          </Marker>
         </Map>
       </div>
     );
-  }
-}
-
-class MyCircle extends React.Component {
-  componentDidMount() {
-    // here we can get the bounds of the circle
-    let b = this.circ.leafletElement.getBounds();
-    //this.props.passBoundsToParent(b);
-  }
-
-  render() {
-    return <Circle center={[this.props.data.lat, this.props.data.lng]} radius={600} fillColor="blue" ref={(elem) => { this.circ = elem }} />
   }
 }
